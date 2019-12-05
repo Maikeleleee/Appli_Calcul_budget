15641798908b43e53ee4744c1ed369cc20c42ff7
Maikeleleee <fabien.dev59440@gmail.com>
2019-12-05 15:00:31 +0100

    k

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
+let depense = [];
 
+
+
+
+let loyerCharge = document.getElementsByClassName('inputDepense');
+
+
+function enter () {
+   
+   for (var i=0; i < loyerCharge.length; i++) {
+      var loyerChamp = loyerCharge[i].value;
+      depense.push(loyerChamp);
+      console.log(depense)
+      document.getElementById('total').innerHTML = "Total = " + depense;
+   }
+   
+}
 
 
 
@@ -8,9 +25,11 @@
 
 function addEpargne () {
    let epargneValue = document.getElementById('inputEpargne').value;
-   let newSpan = document.createElement('span');
+   let newDiv = document.createElement('div');
 
-   newSpan.innerHTML += epargneValue;
+   document.getElementById('containerSaving').appendChild(newDiv) ;
+   newDiv.innerHTML += epargneValue;
 
    //console.log(epargneValue);
 }
+
