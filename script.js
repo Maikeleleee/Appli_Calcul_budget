9b82deb9d99144f2b161abef15edaa1885c84c2f
Maikeleleee <48218260+Maikeleleee@users.noreply.github.com>
2019-12-05 08:58:58 +0100

    maj
    Mise en ligne du projet.

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+function addEpargne () {
+   let epargneValue = document.getElementById('inputEpargne').value;
+   let newSpan = document.createElement('span');
+
+   newSpan.innerHTML += epargneValue;
+
+   //console.log(epargneValue);
+}
